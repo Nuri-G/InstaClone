a056393a8b0bc5425c5dbf9e44a03bb5abb227c3
Nurivan Gomez <nurivan@fb.com>
2022-06-17 11:42:53 -0700

    Implemented likes

--- app/src/main/java/com/codepath/nurivan/instaclone/Post.java
+++ app/src/main/java/com/codepath/nurivan/instaclone/Post.java
@@ -1,20 +1,31 @@
 package com.codepath.nurivan.instaclone;
 
 import android.util.Log;
+import android.util.SparseArray;
 
+import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseClassName;
+import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.util.Date;
+import java.util.List;
 
 @ParseClassName("Post")
 public class Post extends ParseObject {
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_USER = "user";
     public static final String KEY_IMAGE = "image";
+    public static final String KEY_LIKES = "likes";
+    public static final String KEY_LIKED_USERS = "likedUsers";
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
@@ -39,6 +50,39 @@ public class Post extends ParseObject {
     public void setUser(ParseUser user) {
         put(KEY_USER, user);
     }
+
+    public Number getLikes() {
+        return getNumber(KEY_LIKES);
+    }
+
+    public void addLike(ParseUser user) {
+        JSONArray users = getLikedUsers();
+        users.put(user.getObjectId());
+        put(KEY_LIKED_USERS, users);
+        increment(KEY_LIKES);
+    }
+
+    public void decrementLikes(ParseUser user) {
+        JSONArray users = getLikedUsers();
+        for(int i = 0; i < users.length(); i++) {
+            try {
+                if(users.getString(i).equals(user.getObjectId())) {
+                    users.remove(i);
+                    break;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        put(KEY_LIKED_USERS, users);
+        put(KEY_LIKES, getLikes().intValue() - 1);
+    }
+
+    public JSONArray getLikedUsers() {
+        return getJSONArray(KEY_LIKED_USERS);
+    }
+
+
 
     public String getTimeAgo() {
         Date createdAt = this.getCreatedAt();
--- app/src/main/java/com/codepath/nurivan/instaclone/PostAdapter.java
+++ app/src/main/java/com/codepath/nurivan/instaclone/PostAdapter.java
@@ -2,9 +2,12 @@ package com.codepath.nurivan.instaclone;
 
 import android.content.Context;
 import android.content.Intent;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +17,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.codepath.nurivan.instaclone.activities.PostDetailsActivity;
 import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -52,15 +60,19 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
         return posts.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         ImageView ivFeedPost;
         TextView tvDescription;
         TextView tvUsername;
         TextView tvFeedTimestamp;
         ImageView ivProfileCircle;
+        ImageButton ibLike;
+        TextView tvLikes;
+        TextView tvLikeLabel;
 
         boolean pictureOnly;
+        boolean liked;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -69,19 +81,16 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
             tvUsername = itemView.findViewById(R.id.tvUsername);
             tvFeedTimestamp = itemView.findViewById(R.id.tvFeedTimestamp);
             ivProfileCircle = itemView.findViewById(R.id.ivProfileCircle);
+            ibLike = itemView.findViewById(R.id.ibLike);
+            tvLikes = itemView.findViewById(R.id.tvLikes);
+            tvLikeLabel = itemView.findViewById(R.id.tvLikeLabel);
             this.pictureOnly = false;
-            itemView.setOnClickListener(this);
+            this.liked = false;
         }
 
         public ViewHolder(@NonNull View itemView, boolean pictureOnly) {
-            super(itemView);
-            ivFeedPost = itemView.findViewById(R.id.ivFeedPost);
-            tvDescription = itemView.findViewById(R.id.tvDescription);
-            tvUsername = itemView.findViewById(R.id.tvUsername);
-            tvFeedTimestamp = itemView.findViewById(R.id.tvFeedTimestamp);
-            ivProfileCircle = itemView.findViewById(R.id.ivProfileCircle);
+            this(itemView);
             this.pictureOnly = pictureOnly;
-            itemView.setOnClickListener(this);
         }
 
         public void bind(Post post) {
@@ -95,7 +104,62 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
                 tvUsername.setVisibility(View.GONE);
                 tvFeedTimestamp.setVisibility(View.GONE);
                 ivProfileCircle.setVisibility(View.GONE);
+                ibLike.setVisibility(View.GONE);
+                tvLikeLabel.setVisibility(View.GONE);
+
             }
+
+            tvLikes.setText(post.getLikes().toString());
+
+            ivFeedPost.setOnClickListener(e -> {
+                // create intent for the new activity
+                Intent intent = new Intent(context, PostDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Post.class.getSimpleName(), post);
+                // show the activity
+                context.startActivity(intent);
+            });
+
+            JSONArray likedUsers = post.getLikedUsers();
+
+            for(int i = 0; i < likedUsers.length(); i++) {
+                String like = "";
+                try {
+                    like = likedUsers.getString(i);
+                    if(like.equals(ParseUser.getCurrentUser().getObjectId())) {
+                        liked = true;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            setLike();
+
+
+
+            ibLike.setOnClickListener(e -> {
+                liked = !liked;
+
+                setLike();
+
+                if(liked) {
+                    post.addLike(ParseUser.getCurrentUser());
+                } else {
+                    post.decrementLikes(ParseUser.getCurrentUser());
+                }
+
+                tvLikes.setText(post.getLikes().toString());
+
+                post.saveInBackground(err -> {
+                    if(err != null) {
+                        Log.e("", "Failed to save post",  err);
+                    }
+                    if(err == null)
+                        Log.i("ViewHolder", "Saved like.");
+                });
+
+            });
 
             try {
                 Glide.with(context)
@@ -112,21 +176,11 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
                 e.printStackTrace();
             }
         }
-
-        @Override
-        public void onClick(View v) {
-            // gets item position
-            int position = getAdapterPosition();
-            // make sure the position is valid, i.e. actually exists in the view
-            if (position != RecyclerView.NO_POSITION) {
-                // get the movie at the position, this won't work if the class is static
-                Post post = posts.get(position);
-                // create intent for the new activity
-                Intent intent = new Intent(context, PostDetailsActivity.class);
-                // serialize the movie using parceler, use its short name as a key
-                intent.putExtra(Post.class.getSimpleName(), post);
-                // show the activity
-                context.startActivity(intent);
+        private void setLike() {
+            if(liked) {
+                ibLike.setImageResource(R.drawable.ufi_heart_active);
+            } else {
+                ibLike.setImageResource(R.drawable.ufi_heart);
             }
         }
     }
